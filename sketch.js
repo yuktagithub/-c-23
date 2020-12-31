9b8b859fed4f3258498b61f61b830e1b35f54341
Yukta Kdam <73293469+yuktagithub@users.noreply.github.com>
2020-12-31 19:01:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+ var ground1;
+  var box2;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50);
+    ground1 = new ground(200,390,400,30);
+box2 = new Box (240,200,50,50);
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    ground1.display();
+    box2.display();
+   
+}
